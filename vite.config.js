4df7fda69cc66cd898a60b5055fc81f072268f0d
geek <1317948850@qq.com>
2022-11-08 05:53:09 +0800

    修复路由错误提示

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
-import { defineConfig } from 'vite';
+import {
+	defineConfig
+} from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
 import path from "path";
- 
-function resolve(dir) {
-  return path.join(__dirname, dir);
-}
+import h5ProdEffectPlugin from 'uni-vite-plugin-h5-prod-effect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    uni(),
-  ],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, 'src') },
-  },
-  css: {
-    // css预处理器
-    preprocessorOptions: {
-      less: {
-        charset: false,
-        additionalData: '@import "./src/components/css/Layout.less";',
-      },
-    },
-  }
+	plugins: [
+		uni(),
+		// 对h5 production环境打包时的特殊处理，否则uni-crazy-router在这个环境会异常
+		h5ProdEffectPlugin()
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, 'src')
+		},
+	},
+	css: {
+		// css预处理器
+		preprocessorOptions: {
+			less: {
+				charset: false,
+				additionalData: '@import "./src/components/css/Layout.less";',
+			},
+		},
+	}
 })
